e9991c0536775ac82789ce02ba5fd7e15c94e2dc
Jake Weinstein <jake@aospa.co>
2019-09-17 19:44:45 +0200

    Default to legacy power HAL for legacy targets.
    For the new power HAL, set TARGET_USES_NON_LEGACY_POWERHAL
    as true in said devices respective board configuration.
    
    SM6150/SM7150/SM8150 and newer will use the new HIDL HAL by default.
    
    Change-Id: I3d6ef0bf093a333c9c2f1c81518c00bcd39068c9

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -1,8 +1,10 @@
-#Power product definitions
+ifeq ($(TARGET_USES_NON_LEGACY_POWERHAL),true)
+#Power HAL targets.
 PRODUCT_PACKAGES += android.hardware.power@1.2-impl
 PRODUCT_PACKAGES += android.hardware.power@1.2-service
+endif
 
-#Powerhint File
+#PowerHint File.
 ifeq ($(TARGET_BOARD_PLATFORM),msmnile)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),kona)
